1a21ddc0684810317fcac78763eeac0bc56a88e8
unknown <kuzy2508@mail.ru>
2022-11-10 20:31:59 +0300

    Zadacha_29

---
+++ Zadacha_29/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+//Заполнение массива производит пользователь.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+int[] array = new int[8];
+
+Console.WriteLine(array.Length);
+
+for (int i = 0; i < array.Length; i++)
+{
+Console.Write($"Введите элемент массива под номером {i}: ");
+array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+for (int i=0; i < array.Length; i++)
+{
+    Console.Write(array[i] +", ");
+}
